36d53b8b80cd53f4fd357e0b611844e6ab9170af
Jang-sajang <skehwkddngur@nate.com>
2024-08-05 17:03:35 +0900

    08-05

--- SQL_work/0805ex01.sql
+++ SQL_work/0805ex01.sql
@@ -2,65 +2,66 @@ create database highSchool;
 use highschool;
 
 create table Student(
-	   SID varchar(10) auto_increment primary key,
-	   sname varchar(20) not null,
-       syear varchar(5) not null default '2024',
-       sgrade varchar(3) not null ,
-       sclass varchar(3) not null ,
-       snumber varchar(5) not null ,
-       spassword varchar(50) not null DEFAULT '1234' 
+	   s_ID integer auto_increment primary key,
+	   s_name varchar(20) not null,
+       s_year varchar(5) not null default '2024',
+       s_grade varchar(3) not null ,
+       s_class varchar(3) not null ,
+       s_number varchar(5) not null ,
+       s_password varchar(50) not null DEFAULT '1234' 
 );
 
 create table Teacher(
-	   tID varchar(10) not null primary key,
+	   t_ID integer(10) not null primary key,
        password varchar(20) not null,
-       tname varchar(50) not null,
+       t_name varchar(50) not null,
        subject varchar(50) not null,
-       ttype char(1) null default '4'
+       t_type char(1) null default '4'
 );
 
 create table lesson(
-	   LID varchar(10) auto_increment primary key,
-       tid varchar(10) not null,
-       Lname varchar(50) not null,
-       Lyear varchar(5) not null,
-       Lgrade varchar(3) not null,
-       Lsemester char(1) not null,
-       Lclass varchar(3) null,
-       Lday date,
-       Ltime datetime,
-       Lroom varchar(20) null,
-       foreign key(tid) references teacher(tid)
+	   L_ID integer(10) auto_increment primary key,
+       t_id integer(10) not null,
+       L_name varchar(50) not null,
+       L_year varchar(5) not null,
+       L_grade varchar(3) not null,
+       L_semester char(1) not null,
+       L_class varchar(3) null,
+       L_day date,
+       L_time datetime,
+       L_room varchar(20) null,
+       foreign key(t_id) references Teacher(t_ID)
 );
 
 create table AtList(
-	   LID varchar(10) not null ,
-	   SID varchar(10) not null ,
-       primary key(lid, sid),
-       foreign key(sid) references student(sid),
-       foreign key(lid) references lesson(lid)
+	   L_ID integer(10) not null ,
+	   S_ID integer(10) not null ,
+       primary key(l_id, s_id),
+       foreign key(s_id) references student(s_id),
+       foreign key(l_id) references lesson(l_id)
 );
 
 create table perform(
-	   pid varchar(10) auto_increment primary key,
-       ptitle varchar(100) not null,
-       ptype char(1) null,
-       pcontent tinytext not null,
-       pstandard varchar(100) null,
-       pstartdate1 date not null,
-       penddate2 date not null,
-       lid varchar(10) not null,
-       foreign key(lid) references lesson(lid)
+	   p_id integer(10) auto_increment primary key,
+       p_title varchar(100) not null,
+       p_type char(1) null,
+       p_content tinytext not null,
+       p_standard varchar(100) null,
+       p_startdate1 date not null,
+       p_end_date2 date not null,
+       l_id integer(10) not null,
+       foreign key(l_id) references lesson(l_id)
 );
 
 create table evaluation(
-	   sid varchar(10) auto_increment primary key,
-	   pid varchar(10) not null,
+	   s_id integer(10) auto_increment primary key,
+       L_ID integer(10) not null ,
+	   p_id integer(10) not null,
 	   e_score integer not null,
        e_check char(1) null,
        e_chechdate date null,
 --        primary key(lid, pid, sid),
-       foreign key(lid) references lesson(lid),
-       foreign key(pid) references perform(pid),
-       foreign key(sid) references student(sid)
+       foreign key(l_id) references lesson(l_id),
+       foreign key(p_id) references perform(p_id),
+       foreign key(s_id) references student(s_id)
 );
