c64f52a02ce4e36ada23e9fbedd5e77b8dfa804e
DESKTOP-SCJ83VA\user <skehwkddngur@nate.com>
2024-10-22 18:09:29 +0900

    24-10-22

--- vue_work/green_attendance/src/component/SideBanner.vue
+++ vue_work/green_attendance/src/component/SideBanner.vue
@@ -1,8 +1,6 @@
 <template>
   <div
-    class="fixed w-24 lg:w-28 max-sm:hidden top-1/2 right-5 lg:right-8 xl:right-16 transform -translate-y-1/2 
-    bg-white text-blue-900 p-4 rounded-md shadow-lg hover:bg-slate-100 
-    transition-colors duration-200"
+    class="fixed w-24 lg:w-28 max-sm:hidden top-1/2 right-5 lg:right-8 xl:right-16 transform -translate-y-1/2 bg-white text-blue-900 p-4 rounded-md shadow-lg hover:bg-slate-100 transition-colors duration-200"
   >
     <!-- 배너 내용 -->
     <h1 class="text-2xl font-bold text-center">Quick</h1>
@@ -11,42 +9,44 @@
     <!-- 버튼 -->
     <button
       class="block mt-2 bg-white text-blue-900 px-3 py-1 rounded hover:bg-gray-200 transition-colors duration-300"
-      @click="handleClick">
-      <img src="../images/calln.jpg" alt="#" class="w-20">
+      @click="handleClick"
+    >
+      <img src="../images/calln.jpg" alt="#" class="w-20" />
       <p class="text-sm">출결관리</p>
     </button>
     <button
       class="block mt-2 bg-white text-blue-900 px-3 py-1 rounded hover:bg-gray-200 transition-colors duration-300"
-      @click="handleClick">
-      <img src="../images/Ques.jpg" alt="#" class="w-20">
+      @click="handleClick"
+    >
+      <img src="../images/Ques.jpg" alt="#" class="w-20" />
       <p class="text-sm">문의사항</p>
     </button>
     <button
       class="block mt-2 bg-white text-blue-900 px-3 py-1 rounded hover:bg-gray-200 transition-colors duration-300"
-      @click="handleClick">
-      <img src="../images/homeIcon.jpg" alt="#" class="w-20">
+      @click="handleClick"
+    >
+      <img src="../images/homeIcon.jpg" alt="#" class="w-20" />
       <p class="text-sm">내페이지</p>
     </button>
     <button
       class="block mt-2 bg-white text-blue-900 px-3 py-1 rounded hover:bg-gray-200 transition-colors duration-300"
-      @click="handleClick">
-      <img src="../images/napal.jpg" alt="#" class="w-20">
+      @click="handleClick"
+    >
+      <img src="../images/napal.jpg" alt="#" class="w-20" />
       <p class="text-sm">알림마당</p>
     </button>
   </div>
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const handleClick= () => {
-console.log('hi')
-  router.push({ name: 'mypage'})
-};
-
-
+const handleClick = () => {
+  console.log('hi')
+  router.push({ name: 'mypage' })
+}
 </script>
 
 <style scoped>
--- vue_work/green_attendance/src/views/MyPage.vue
+++ vue_work/green_attendance/src/views/MyPage.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>
-<p>Mypage 개발중</p>
+  <SideBar class="absolute"></SideBar>
+  <h1 class="mx-60 py-4 text-blue-800 font-bold">마이페이지</h1>
+  <hr class="w-2/3 mx-auto" />
+
+  <div class="flex justify-center items-start p-6">
+    <!-- 사진 영역 -->
+    <aside>
+      <img src="../images/stude.jpg" alt="#" class="w-40 flex" />
+    </aside>
+
+    <!-- 사용자 정보 영역 -->
+    <div class="px-10 space-y-4">
+      <!-- 이름 -->
+      <div class="flex">
+        <span class="font-bold text-gray-800 w-24">이름 </span>
+        <span class="text-gray-500">김철수</span>
+      </div>
+      <!-- 전화번호 -->
+      <div class="flex">
+        <span class="font-bold text-gray-800 w-24">전화번호</span>
+        <span class="text-gray-500">010-XXXX-XXXX</span>
+      </div>
+      <!-- 이메일 -->
+      <div class="flex">
+        <span class="font-bold text-gray-800 w-24">e-mail</span>
+        <span class="text-gray-500">없음</span>
+      </div>
     </div>
+  </div>
+
+  <hr class="border-2 border-b-blue-900 w-2/3 mx-auto" />
+  <h1 class="mx-60 py-4 text-blue-800 font-bold">수강중인 강좌</h1>
+  <div class="mx-60">
+    <div class="flex">
+      강좌1
+      <input type="text" name="강의정보" id="" class="border" />
+    </div>
+    <div class="flex">강좌2</div>
+    <input type="text" name="강의정보" id="" class="border" />
+  </div>
 </template>
 
 <script setup>
-
+import SideBar from '@/component/SideBar.vue'
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
